3371ca77cc120c953e137f3ec8972831800b5a55
[surferjreb] <[surferjreb@live.com]>
2022-05-09 16:09:00 -0400

    update process

--- src/main/java/com/brownj/eventhandlers/EventController.java
+++ src/main/java/com/brownj/eventhandlers/EventController.java
@@ -5,6 +5,7 @@
 
 package com.brownj.eventhandlers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -35,8 +36,12 @@ public class EventController {
     private CheckBox clearTheFields;
 
     @FXML
+    private Label completeLabel;
+
+    @FXML
     public void initialize(){
         copyButton.setDisable(true);
+        completeLabel.setVisible(false);
     }
 
     @FXML
@@ -51,42 +56,59 @@ public class EventController {
             // Create Path object for each string
             Path copied = Paths.get(fileName1);
             Path originalPath = Paths.get(fileName);
+            String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+            System.out.println("On thread: " + s);
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    try {
 
+                        if (copyFile.isSelected()) {
+                            // Creates copy of File
+                            Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
+                        } else {
+                            /* If file is a Directory, create the directory and iterate over the files.
+                             * Determine if any of those files are directories.
+                             * If it is another Directory, create it and iterate over its contents and copy the files.
+                             */
+                            DirectoryStream<Path> contents = Files.newDirectoryStream(originalPath);
+                            if(!Files.isDirectory(copied)){
+                                Files.createDirectories(copied);
+                            }
 
-            if (copyFile.isSelected()) {
-                // Creates copy of File
-                Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
-            } else {
-                /* If file is a Directory, create the directory and iterate over the files.
-                 * Determine if any of those files are directories.
-                 * If it is another Directory, create it and iterate over its contents and copy the files.
-                 */
-                DirectoryStream<Path> contents = Files.newDirectoryStream(originalPath);
-                if(!Files.isDirectory(copied)){
-                    Files.createDirectories(copied);
-                }
+                            for (Path filePath : contents) {
 
-                for (Path filePath : contents) {
-                    String newPathStr = copied.toString() + "/" + filePath.getFileName();
-                    Path newPath = Paths.get(newPathStr);
-                    if (!Files.isDirectory(filePath)) {
-                        Files.copy(filePath, newPath, StandardCopyOption.REPLACE_EXISTING);
-                    } else {
-                        Files.createDirectories(newPath);
-                        DirectoryStream<Path> tempContents = Files.newDirectoryStream(filePath);
-                        for(Path newFile : tempContents) {
-                            Path tempPath = Paths.get(newPath.toString() + "/" + newFile.getFileName());
-                            Files.copy(newFile, tempPath, StandardCopyOption.REPLACE_EXISTING);
+                                if (!Files.isDirectory(filePath)) {
+                                    Files.copy(filePath, newPath, StandardCopyOption.REPLACE_EXISTING);
+                                } else {
+                                    Files.createDirectories(newPath);
+                                    DirectoryStream<Path> tempContents = Files.newDirectoryStream(filePath);
+                                    for(Path newFile : tempContents) {
+                                        Path tempPath = Paths.get(newPath.toString() + "/" + newFile.getFileName());
+                                        Files.copy(newFile, tempPath, StandardCopyOption.REPLACE_EXISTING);
+                                    }
+                                }
+                            }
+
                         }
+                        if(clearTheFields.isSelected()) {
+                            pathToCopyFrom.clear();
+                            pathToCopyTo.clear();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
+            };
 
+            if(completeLabel.isVisible()) {
+                completeLabel.setVisible(false);
             }
-            if(clearTheFields.isSelected()) {
-                pathToCopyFrom.clear();
-                pathToCopyTo.clear();
-            }
-        } catch (IOException e) {
+
+            new Thread(task).start();
+            completeLabel.setVisible(true);
+
+        } catch (Exception e) {
             // IOException
             // NoSuchFileException
             e.printStackTrace();
@@ -100,11 +122,11 @@ public class EventController {
         String fileName = pathToCopyFrom.getText();
         boolean disableButtons = fileName.isEmpty() || fileName.trim().isEmpty();
         copyButton.setDisable(disableButtons);
+        completeLabel.setVisible(false);
 
     }
 
-    public void handleChange() {
-        System.out.println("The checkbox is " + (clearTheFields.isSelected() ? "checked" : "not checked"));
-    }
-    
+    // A way to identify which thread you are on.
+    // String s = Platform.isFxApplicationThread() ? "UI Thread" : "Background Thread";
+    // System.out.println("On thread: " + s);
 }
